4cc53554e4719e08c810cb291c81829215eec01d
Richard Fan <richardfan1126@gmail.com>
2016-09-14 22:22:15 +0800

    Update XAPKReader.java

--- src/android/XAPKReader.java
+++ src/android/XAPKReader.java
@@ -1,14 +1,29 @@
 package com.flyingsoftgames.xapkreader;
 
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
-
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.res.AssetFileDescriptor;
 import android.net.Uri;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.android.vending.expansion.zipfile.APKExpansionSupport;
+import com.android.vending.expansion.zipfile.ZipResourceFile;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
 
 public class XAPKReader extends CordovaPlugin {
  @Override public void initialize (final CordovaInterface cordova, CordovaWebView webView) {
@@ -58,10 +73,110 @@ public class XAPKReader extends CordovaPlugin {
     Context context = cordova.getActivity().getApplicationContext();
     Intent intent = new Intent(context, XAPKDownloaderActivity.class);
     intent.putExtras (bundle);
-    //cordova.getActivity().startActivity (intent);
+//    cordova.getActivity().startActivity (intent);
    }
   });
   
   super.initialize (cordova, webView);
+ }
+
+
+    private int mainVersion = 1;
+
+    private int patchVersion = 1;
+
+ /**
+  * Executes the request.
+  *
+  * This method is called from the WebView thread. To do a non-trivial amount of work, use:
+  *     cordova.getThreadPool().execute(runnable);
+  *
+  * To run on the UI thread, use:
+  *     cordova.getActivity().runOnUiThread(runnable);
+  *
+  * @param action          The action to execute.
+  * @param args            The exec() arguments.
+  * @param callbackContext The callback context used when calling back into JavaScript.
+  * @return                Whether the action was valid.
+  * @throws JSONException
+  *
+  * @sa https://github.com/apache/cordova-android/blob/master/framework/src/org/apache/cordova/CordovaPlugin.java
+  */
+ @Override
+ public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+  if (action.equals("get")) {
+   final String filename = args.getString(0);
+   final Context ctx = cordova.getActivity().getApplicationContext();
+   cordova.getThreadPool().execute(new Runnable() {
+    public void run() {
+     try {
+      // Read file
+      PluginResult result = XAPKReader.readFile(ctx, filename, mainVersion, patchVersion, PluginResult.MESSAGE_TYPE_ARRAYBUFFER);
+      callbackContext.sendPluginResult(result);
+     }
+     catch(Exception e) {
+      e.printStackTrace();
+      callbackContext.error(e.getLocalizedMessage());
+     }
+    }
+   });
+   return true;
+  }
+  return false;
+ }
+
+ /**
+  * Read file in APK Expansion file.
+  *
+  * @param ctx      The context of the main Activity.
+  * @param filename The filename to read
+  * @return         PluginResult
+  */
+ private static PluginResult readFile(Context ctx, String filename, int mainVersion, int patchVersion, final int resultType) throws IOException {
+  // Get APKExpensionFile
+  ZipResourceFile expansionFile = APKExpansionSupport.getAPKExpansionZipFile(ctx, mainVersion, patchVersion);
+
+  if (null == expansionFile) {
+   throw new IOException("APKExpansionFile not found.");
+  }
+
+  // Find file in ExpansionFile
+  AssetFileDescriptor fileDescriptor = expansionFile.getAssetFileDescriptor(filename);
+
+  if (null == fileDescriptor) {
+   throw new IOException("File not found (" + filename + ").");
+  }
+
+  // Read file
+  InputStream inputStream = fileDescriptor.createInputStream();
+  ByteArrayOutputStream os = new ByteArrayOutputStream();
+  byte[] buffer = new byte[1024];
+  int read = 0;
+  while ((read = inputStream.read(buffer, 0, buffer.length)) != -1) {
+   os.write(buffer, 0, read);
+  }
+  os.flush();
+
+  // get file content type
+  String contentType = URLConnection.guessContentTypeFromStream(inputStream);
+
+  PluginResult result;
+  switch (resultType) {
+   case PluginResult.MESSAGE_TYPE_STRING:
+    result = new PluginResult(PluginResult.Status.OK, os.toString("UTF-8"));
+    break;
+   case PluginResult.MESSAGE_TYPE_ARRAYBUFFER:
+    result = new PluginResult(PluginResult.Status.OK, os.toByteArray());
+    break;
+   case PluginResult.MESSAGE_TYPE_BINARYSTRING:
+    result = new PluginResult(PluginResult.Status.OK, os.toByteArray(), true);
+    break;
+   default: // Base64.
+    byte[] base64 = Base64.encode(os.toByteArray(), Base64.NO_WRAP);
+    String s = "data:" + contentType + ";base64," + new String(base64, "US-ASCII");
+    result = new PluginResult(PluginResult.Status.OK, s);
+  }
+
+  return result;
  }
 }
